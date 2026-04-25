c2f732a88188fa15932b4ddd65da7239cd264f74
krissia1204 <issi122404@gmail.com>
2026-04-25 17:44:14 -0600

    Subiendo proyecto bluej

---
+++ Hospital/Paciente.java
@@ -0,0 +1,29 @@
+
+
+public class Paciente
+{
+    private String nombre;
+    private int edad;
+    private String espcRequerida;
+    private String nvlUrgencia;
+    private Doctor docAsignado;
+    /**
+     * Constructor for objects of class Paciente
+     */
+    public Paciente(String nom, int laEdad, String especialidad,String nivel )
+    {
+        this.nombre= nom;
+        this.edad= laEdad;
+        this.espcRequerida= especialidad;
+        this.nvlUrgencia= nivel;
+        this.docAsignado= null;
+       
+    }
+
+   
+    public String getNombre()
+    {
+        
+        return this.nombre;
+    }
+}
