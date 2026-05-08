c2857e65695457520c35e630749c30d1d08b0227
krissia1204 <issi122404@gmail.com>
2026-05-07 23:58:02 -0600

    Cifrador integrado

--- Hospital/Cifrador.java
+++ Hospital/Cifrador.java
@@ -2,12 +2,52 @@
 
 public class Cifrador
 {   
-    private int [][] cifrado;
+    private final int [][] cifrado;
     
     public Cifrador()
     {
-       this.cifrado= new int[9][9];
+       this.cifrado= new int [3][2];
+       
+       this.cifrado[0][0]=89; 
+       this.cifrado[0][1]=76; 
+       this.cifrado[1][0]=42;
+       this.cifrado[1][1]=54;
+       this.cifrado[2][0]= 33;
+       this.cifrado[2][1]= 35;
     }
+    
+    public String cifrar(String textoPlano){
+       char[] texto= textoPlano.toCharArray(); 
+       String resultado= "";
+       
+       for(int i=0; i<texto.length;i++){
+           char actual= texto[i];
+           
+           //convertir a ascii
+           int vAscii = (int) actual;
+           
+           //sustitución
+           
+           int fila= vAscii%3;
+           int col= vAscii%2;
+           
+           int textCifrado= cifrado[fila][col];
+           
+           resultado+= (char) textCifrado;
+           
+           
+       }
+       return resultado;
+    }
+    
+    public boolean verificar(String usuario, String intentoPass,String usuarioSistema, String passSistema){
+       String tryCifrado= this.cifrar(intentoPass);
+       
+       return tryCifrado.equals(passSistema)&&usuario.equals(usuarioSistema);
+        
+    }
+    
+    
 
     
 }
--- Hospital/Hospital.java
+++ Hospital/Hospital.java
@@ -63,7 +63,7 @@ public class Hospital
             System.out.println("Paciente ingresado exitosamente");
         }
         
-        else if (p.getNvlUrgencia().equals("Critico")&&!camaEncontrada&&doctorAsignado!=null){
+        else if (p.getNvlUrgencia().equals("CRITICO")&&!camaEncontrada&&doctorAsignado!=null){
              int filaEncontradaPC= -1;
              int columnaEncontradaPC= -1;
               boolean camaEncontradaPC= false;
